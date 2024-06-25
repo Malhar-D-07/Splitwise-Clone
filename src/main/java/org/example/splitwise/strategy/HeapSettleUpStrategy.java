38b2af3d078f3d0007d22df7df1a87c6d4618254
Malhar <malhar@higgsbosonhealth.com>
2024-06-25 20:13:14 +0530

    Added code for optimising transactions

--- src/main/java/org/example/splitwise/strategy/HeapSettleUpStrategy.java
+++ src/main/java/org/example/splitwise/strategy/HeapSettleUpStrategy.java
@@ -1,14 +1,26 @@
 package org.example.splitwise.strategy;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.example.splitwise.models.Expense;
 import org.example.splitwise.models.ExpenseUser;
 import org.example.splitwise.models.User;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
+@Getter
+@Setter
+class Pair {
+    private String name;
+    private Double amount;
+
+    public Pair(String name, Double amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+}
 
 @Component
 public class HeapSettleUpStrategy implements SettleUpStrategy {
@@ -19,21 +31,47 @@ public class HeapSettleUpStrategy implements SettleUpStrategy {
         Map<String, Double> expenseMap = new HashMap<>();
         for (Expense expense: expenses) {
             User expenseDoneBy = expense.getCreatedBy();
-            expenseMap.put(expenseDoneBy.getName(), expenseMap.getOrDefault(expenseDoneBy.getName(), (double) 0) + expense.getAmount());
+            expenseMap.put(expenseDoneBy.getName(), expenseMap.getOrDefault(expenseDoneBy.getName(), 0.0) + expense.getAmount());
 
             for (ExpenseUser expenseUser: expense.getExpenseUsers()) {
-                if (expenseMap.containsKey(expenseUser.getUser().getName())) {
-                    Double newAmount = expenseMap.getOrDefault(expenseUser.getUser().getName(), (double) 0) - expenseUser.getAmount();
-                    expenseMap.put(expenseUser.getUser().getName(), newAmount);
-                }
-                else {
-                    Double existingAmount = expenseMap.getOrDefault(expenseUser.getUser().getName(), (double) 0);
-                    expenseMap.put(expenseUser.getUser().getName(), existingAmount - expenseUser.getAmount());
-                }
+                Double newAmount = expenseMap.getOrDefault(expenseUser.getUser().getName(), 0.0) - expenseUser.getAmount();
+                expenseMap.put(expenseUser.getUser().getName(), newAmount);
+            }
+        }
+
+        PriorityQueue<Pair> minHeap = new PriorityQueue<>(Comparator.comparingDouble(Pair::getAmount));
+        PriorityQueue<Pair> maxHeap = new PriorityQueue<>((a, b) -> Double.compare(b.getAmount(), a.getAmount()));
+
+        for (Map.Entry<String, Double> entry : expenseMap.entrySet()) {
+            if (entry.getValue() < 0) {
+                minHeap.offer(new Pair(entry.getKey(), entry.getValue()));
+            } else if (entry.getValue() > 0) {
+                maxHeap.offer(new Pair(entry.getKey(), entry.getValue()));
+            }
+        }
+
+        List<String> transactions = new ArrayList<>();
+
+        while (!minHeap.isEmpty() && !maxHeap.isEmpty()) {
+            Pair debtor = minHeap.poll();
+            Pair creditor = maxHeap.poll();
+
+            double settleAmount = Math.min(-debtor.getAmount(), creditor.getAmount());
+            transactions.add(debtor.getName() + " pays " + settleAmount + " to " + creditor.getName());
+
+            debtor.setAmount(debtor.getAmount() + settleAmount);
+            creditor.setAmount(creditor.getAmount() - settleAmount);
+
+            if (debtor.getAmount() < 0) {
+                minHeap.offer(debtor);
             }
 
+            if (creditor.getAmount() > 0) {
+                maxHeap.offer(creditor);
+            }
         }
-        System.out.println("Final Map: " + expenseMap.toString());
+
+        System.out.println("Transactions: " + transactions);
         return null;
     }
 }
